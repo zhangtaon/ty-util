517522c261564f79d61bc732983f423b22cf9fe6
张涛 <zhangtao@tsingyun.net>
2020-04-11 19:29:39 +0800

    添加类型检测/日志输出/树结构解析

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ import './lib/confirmDelete'
 import './lib/matchPropValue'
 export * from "./lib/exportExcel"
 export * from "./lib/log"
+export * from "./lib/treeParse"
+export * from "./lib/isType"
 
