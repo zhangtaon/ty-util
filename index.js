3f71e0b23a946e58f0bbc3fc750a03c5bd304c83
zto <zhangtao@tsingyun.net>
2020-05-06 15:25:46 +0800

    添加克隆及差汇总啊根基点

--- index.js
+++ index.js
@@ -5,8 +5,10 @@
 import './lib/confirmDelete'
 import './lib/matchPropValue'
 export * from "./lib/exportExcel"
+export * from "./lib/getParentNode"
 export * from "./lib/log"
 export * from "./lib/treeParse"
+export * from "./lib/clone"
 export * from "./lib/isType"
 export * from "./lib/debounce"
 
